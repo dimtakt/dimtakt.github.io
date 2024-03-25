a74c5dc7a18fcab01e8ba03ffd43b3a6892e23f9
SDTR <sndvsdtr@gmail.com>
2024-03-25 18:50:12 +0900

    profile_pr01

---
+++ profile/index.js
@@ -0,0 +1,10 @@
+// 링크 연결
+$(".c_discord").on("click", function () {
+    $(location).attr("href", "discord://discord.com/users/150166254290862080")
+})
+$(".c_steam").on("click", function () {
+    $(location).attr("href", "https://steamcommunity.com/id/1951929/")
+})
+$(".c_github").on("click", function () {
+    $(location).attr("href", "https://github.com/dimtakt")
+})
