d9f075227dcea1d8871706ed3286149eb39f5bc2
SDTR <sndvsdtr@gmail.com>
2024-04-03 08:37:01 +0900

    add auto refresh spotify profile

--- profile/index.js
+++ profile/index.js
@@ -12,5 +12,12 @@ $(".spotify_link").on("click", function () {
     location.href = "https://spotify-github-profile.vercel.app/api/view?uid=wagt04j2x2k2rbriz8t6fj9l8&redirect=true"
 })
 
+// 5초마다 자동 갱신
+// 더미 랜덤값을 붙이는 식으로 다른 페이지를 불러오는 것처럼 속여서 새로고침함
+setInterval(() => {
+    let image_url = $(".spotify_link").attr("src")
+    $(".spotify_link").attr("src", image_url + `?v=${Math.random()}`)
+}, 5000);
+
 
 console.log("멀봐 ㅡㅡ")
