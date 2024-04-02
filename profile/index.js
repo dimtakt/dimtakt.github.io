6603f2f3ef379aca45bb3c5c17f92d55d2b7cc4b
SDTR <sndvsdtr@gmail.com>
2024-04-03 07:14:12 +0900

    .

--- profile/index.js
+++ profile/index.js
@@ -1,15 +1,15 @@
 // 링크 연결
 $(".c_discord").on("click", function () {
-    location.href="discord://discord.com/users/150166254290862080"
+    location.href = "discord://discord.com/users/150166254290862080"
 })
 $(".c_steam").on("click", function () {
-    location.href="https://steamcommunity.com/id/1951929/"
+    location.href = "https://steamcommunity.com/id/1951929/"
 })
 $(".c_github").on("click", function () {
-    location.href="https://github.com/dimtakt"
+    location.href = "https://github.com/dimtakt"
 })
 $(".spotify_link").on("click", function () {
-    location.href="https://spotify-github-profile.vercel.app/api/view?uid=wagt04j2x2k2rbriz8t6fj9l8&redirect=true"
+    location.href = "https://spotify-github-profile.vercel.app/api/view?uid=wagt04j2x2k2rbriz8t6fj9l8&redirect=true"
 })
 
 
