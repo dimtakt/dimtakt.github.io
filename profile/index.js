6a95a6bf57fb518f38462c5eb09923577e21f296
SDTR <sndvsdtr@gmail.com>
2024-04-03 12:17:44 +0900

    .

--- profile/index.js
+++ profile/index.js
@@ -15,8 +15,19 @@ $(".spotify_link").on("click", function () {
 // 5초마다 자동 갱신
 // 더미 랜덤값을 붙이는 식으로 다른 페이지를 불러오는 것처럼 속여서 새로고침함
 setInterval(() => {
-    let image_url = $(".spotify_link").attr("src")
-    $(".spotify_link").attr("src", image_url + `?v=${Math.random()}`)
+    // 초를 이용, 짝수 초마다 url 초기화
+    // ?v= 랜덤 문자열 계속 쌓이는 것 방지
+    let now = new Date();
+    let sec = now.getSeconds();
+
+    if (sec % 2) {
+        let image_url = $(".spotify_link").attr("src")
+        $(".spotify_link").attr("src", image_url + `?v=${Math.random()}`)
+    }
+    else {
+        // 초기화
+        $(".spotify_link").attr("src", 'https://spotify-github-profile.vercel.app/api/view?uid=wagt04j2x2k2rbriz8t6fj9l8&cover_image=true&theme=natemoo-re&show_offline=false&background_color=121212&interchange=true&bar_color=ffffff&bar_color_cover=false')
+    }
 }, 5000);
 
 
